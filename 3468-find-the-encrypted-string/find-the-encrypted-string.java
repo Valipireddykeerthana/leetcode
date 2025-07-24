6b25a61523feb4922d9f574f301409bd5bad706a
Valipireddykeerthana <keerthanavalipireddy@gmail.com>
2025-07-24 15:47:48 +0530

    Time: 1 ms (100.00%) | Memory: 42.9 MB (33.82%) - LeetSync

--- 3468-find-the-encrypted-string/find-the-encrypted-string.java
+++ 3468-find-the-encrypted-string/find-the-encrypted-string.java
@@ -2,6 +2,7 @@ class Solution {
     public String getEncryptedString(String s, int k) {
         int n=s.length();
         k=k%n;
+        //substring is used to letters between them
         return (s+s).substring(k,k+n);
        
 
