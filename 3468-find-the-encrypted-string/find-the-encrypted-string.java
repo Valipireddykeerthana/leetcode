da4bed3a26b72e996fa72856cbdfa9a62f478757
Valipireddykeerthana <keerthanavalipireddy@gmail.com>
2025-07-24 15:45:59 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (93.82%) - LeetSync

---
+++ 3468-find-the-encrypted-string/find-the-encrypted-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String getEncryptedString(String s, int k) {
+        int n=s.length();
+        k=k%n;
+        return (s+s).substring(k,k+n);
+       
+
+        
+
+        
+    }
+}
