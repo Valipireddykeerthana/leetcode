64bad379c119db0f14911c368e5985ea8eec11a7
Valipireddykeerthana <keerthanavalipireddy@gmail.com>
2025-07-30 10:33:01 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (13.94%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -3,8 +3,8 @@ class Solution {
         // 2*n is used to represent the size of result array
         int [] result=new int[2*n];
         for(int i=0;i<n;i++){
-            result[2*i]=nums[i];
-            result[2*i+1]=nums[i+n];
+            result[2*i]=nums[i];//return i values in even inndex of result
+            result[2*i+1]=nums[i+n];//return i values in odd index of result
             }
             
         return result;
