54189f45be5105e7c6f81f73355dd74d9be1aec8
Valipireddykeerthana <keerthanavalipireddy@gmail.com>
2025-08-12 10:45:45 +0530

    Time: 1 ms (100.00%) | Memory: 43 MB (8.01%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,13 +1,13 @@
 class Solution {
     public int myAtoi(String s) {
-        s=s.trim();
+        s=s.trim(); //remove spaces
         if(s.isEmpty()){
             return 0;
         }
         int sign=1;
         long result=0;
         int i=0;
-        if(s.charAt(0)=='-'){
+        if(s.charAt(0)=='-'){ //if index 0 has - keep the result as -
             sign= -1;
             i++;
         }
@@ -15,10 +15,13 @@ class Solution {
             i++;
         }
         while(i<s.length() && Character.isDigit(s.charAt(i))){
+            //in case we donot traversed at last and we have digits in ith index
             result=result*10+(s.charAt(i)-'0');
+            //0*10+4=4
+            //4*10+2=42
         
-        if(sign*result>Integer.MAX_VALUE) return Integer.MAX_VALUE;
-        if(sign*result<Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        if(sign*result>Integer.MAX_VALUE) return Integer.MAX_VALUE;//checking the number is greater than 2^31
+        if(sign*result<Integer.MIN_VALUE) return Integer.MIN_VALUE;//checking the number is less than -2^31
         i++;
         }
         return (int)(sign*result);
