297c5ac0ffd10cf1346b365f8ee2030fa816c3eb
Valipireddykeerthana <keerthanavalipireddy@gmail.com>
2025-08-18 11:14:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (93.63%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -3,7 +3,6 @@ class Solution {
         int ans=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=val){
-                
                 nums[ans]=nums[i];
                 ans++;
             }
