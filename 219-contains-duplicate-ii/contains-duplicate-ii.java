bc8a20e1793b8f7a8ebb64261458b5d940a35df1
Valipireddykeerthana <keerthanavalipireddy@gmail.com>
2025-08-29 14:04:16 +0530

    Time: 20 ms (92.59%) | Memory: 59.9 MB (34.98%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -2,12 +2,12 @@ class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                if(i-map.get(nums[i])<=k){
+            if(map.containsKey(nums[i])){ //checks the table contains no.
+                if(i-map.get(nums[i])<=k){ //present index-previous index is less and equal to k
                     return true;
                 }
             }
-            map.put(nums[i],i);
+            map.put(nums[i],i);  //if no. is not there insert the no. and its index in to the table
         }
         return false;
     }
