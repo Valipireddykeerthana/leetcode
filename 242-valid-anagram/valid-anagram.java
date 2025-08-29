d9e4a047937d1f8622f8fd85d8b03c27573e8fca
Valipireddykeerthana <keerthanavalipireddy@gmail.com>
2025-08-29 15:02:27 +0530

    Time: 16 ms (22.11%) | Memory: 45 MB (16.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,11 +3,17 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        char[] a=s.toCharArray();
-        char [] b=t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a,b);
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+
+        }
+        for(char c:t.toCharArray()){
+            if(!map.containsKey(c)) return false;
+            map.put(c,map.get(c)-1);
+            if(map.get(c)==0) map.remove(c);
+        }
+        return map.isEmpty();
         
     }
 }
