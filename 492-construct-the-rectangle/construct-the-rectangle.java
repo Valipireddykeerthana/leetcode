5cf982663f3ef6cd198ab590f9ffe2ed0fa56d05
Valipireddykeerthana <keerthanavalipireddy@gmail.com>
2025-08-30 14:18:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (31.53%) - LeetSync

--- 492-construct-the-rectangle/construct-the-rectangle.java
+++ 492-construct-the-rectangle/construct-the-rectangle.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] constructRectangle(int area) {
-        int b=(int)Math.sqrt(area);
-        while(area%b!=0){
-            b--;
+        int b=(int)Math.sqrt(area);  //suppose area is 36 square root 6
+        while(area%b!=0){ //if 36 divide by 6 thent the remainder is 0
+            b--; // if not then decrease the 6 that means 5,4,3,2,1
         }
-        int l=area/b;
-        int[] ans=new int[2];
+        int l=area/b; // 36/6 is 6
+        int[] ans=new int[2];// then add to array
         ans[0]=l;
         ans[1]=b;
         return ans;
