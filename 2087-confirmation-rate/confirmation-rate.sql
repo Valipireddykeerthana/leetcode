493ffaf798b3f52addd91a82e40be9f3ffdfae2d
Valipireddykeerthana <keerthanavalipireddy@gmail.com>
2025-12-11 15:36:27 +0530

    Time: 5290 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select s.user_id, 
+round(ifnull(sum(c.action='confirmed')/sum(c.action in('confirmed','timeout')), 0), 2)
+as confirmation_rate
+from signups s
+left join confirmations c
+on s.user_id=c.user_id
+group by s.user_id;
